8ee85590a16160bbfcf91bc399a2e73fe7a4301d
Prof Peter Kille <33253889+Peter-Kille@users.noreply.github.com>
2026-02-02 14:37:49 +0000

    Update 3B-kraken_array.sh 100GB

--- modules/3B-kraken_array.sh
+++ modules/3B-kraken_array.sh
@@ -2,7 +2,7 @@
 #SBATCH --nodes=1              # number of nodes to use
 #SBATCH --tasks-per-node=1     #
 #SBATCH --cpus-per-task=8      #   
-#SBATCH --mem=64000            # in megabytes, unless unit explicitly stated
+#SBATCH --mem=100000            # in megabytes, unless unit explicitly stated
 
 echo "Some Usable Environment Variables:"
 echo "================================="
